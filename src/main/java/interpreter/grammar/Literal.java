ce23276471f76c197faddf79244bca7810d1284e
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-08 22:23:54 +0100

    refactor: use a sealed class for literals

---
+++ src/main/java/interpreter/grammar/Literal.java
@@ -0,0 +1,51 @@
+package interpreter.grammar;
+
+import lombok.NonNull;
+
+public sealed interface Literal {
+
+	java.lang.String format();
+
+	public record Nil() implements Literal {
+
+		@Override
+		public java.lang.String format() {
+			return "null";
+		}
+
+	}
+
+	public record Boolean(
+		boolean value
+	) implements Literal {
+
+		@Override
+		public java.lang.String format() {
+			return java.lang.Boolean.toString(value);
+		}
+
+	}
+
+	public record String(
+		@NonNull java.lang.String value
+	) implements Literal {
+
+		@Override
+		public java.lang.String format() {
+			return value;
+		}
+
+	}
+
+	public record Number(
+		double value
+	) implements Literal {
+
+		@Override
+		public java.lang.String format() {
+			return Double.toString(value);
+		}
+
+	}
+
+}
