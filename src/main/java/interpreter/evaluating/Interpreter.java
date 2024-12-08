1455873cbbd2f8920ec1ca39bb33643886d0f552
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-09 00:25:08 +0100

    pass 4th evaluating-expressions

--- src/main/java/interpreter/evaluating/Interpreter.java
+++ src/main/java/interpreter/evaluating/Interpreter.java
@@ -30,12 +30,32 @@ public class Interpreter implements Expression.Visitor<Value> {
 
 	@Override
 	public Value visitUnary(Expression.Unary unary) {
-		throw new UnsupportedOperationException();
+		final var right = evaluate(unary.right());
+
+		return switch (unary.operator().type()) {
+			case BANG -> new Value.Boolean(!isTruthy(right));
+			case MINUS -> {
+				if (right instanceof Value.Number(final var value)) {
+					yield new Value.Number(-value);
+				}
+
+				throw new UnsupportedOperationException();
+			}
+			default -> throw new UnsupportedOperationException();
+		};
 	}
 
 	@Override
 	public Value visitBinary(Expression.Binary binary) {
 		throw new UnsupportedOperationException();
+	}
+
+	public boolean isTruthy(Value value) {
+		return switch (value) {
+			case Value.Nil __ -> false;
+			case Value.Boolean(final var rawValue) -> rawValue;
+			default -> true;
+		};
 	}
 
 }
